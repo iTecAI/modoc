165f45354396dd10e3185730d06552cade9d7861
Dax Harris <dax.harris@bryx.com>
2022-08-17 09:20:25 -0400

    some basic rendering functions

--- modoc/src/renderers/MuiRenderer.tsx
+++ modoc/src/renderers/MuiRenderer.tsx
@@ -1,20 +1,70 @@
 import RenderParser from "../renderParser";
 import React from "react";
 import { AllRenderItems } from "../types";
-import { RenderGroupItem } from "../types/renderTypes";
+import {
+    RenderDividerItem,
+    RenderGroupItem,
+    RenderTextItem
+} from "../types/renderTypes";
+import { Divider, Typography } from "@mui/material";
 
 export default class MuiRenderParser extends RenderParser {
     public SELF_CONSTRUCTOR: typeof RenderParser = MuiRenderParser;
     public renderers: {
         [key: string]: (children: JSX.Element[], object: any) => JSX.Element;
     } = {
-        group: this.renderGroup
+        group: this.renderGroup,
+        text: this.renderText,
+        divider: this.renderDivider
     };
 
     public renderGroup(
         children: JSX.Element[],
-        object: RenderGroupItem
+        _: RenderGroupItem
     ): JSX.Element {
         return <div className="modoc_mui-group">{children}</div>;
+    }
+
+    public renderText(_: JSX.Element[], object: RenderTextItem): JSX.Element {
+        return (
+            <Typography
+                variant={object.textType}
+                sx={{
+                    fontWeight: object.style.includes("bold") ? 400 : undefined,
+                    textDecorationLine: [
+                        object.style.includes("underline") ? "underline" : "",
+                        object.style.includes("strikethrough")
+                            ? "line-through"
+                            : ""
+                    ]
+                        .join(" ")
+                        .trim(),
+                    fontStyle: object.style.includes("italic")
+                        ? "italic"
+                        : undefined
+                }}
+            >
+                {object.text}
+            </Typography>
+        );
+    }
+
+    public renderDivider(
+        children: JSX.Element[],
+        object: RenderDividerItem
+    ): JSX.Element {
+        return (
+            <Divider
+                variant={
+                    {
+                        full: "fullWidth",
+                        inset: "inset",
+                        middle: "middle"
+                    }[object.variant] as any
+                }
+            >
+                {children}
+            </Divider>
+        );
     }
 }
--- modoc/src/types/renderTypes.ts
+++ modoc/src/types/renderTypes.ts
@@ -11,12 +11,25 @@ export type RenderTextItem = {
     supertype: "render";
     type: "text";
     text: ValueItem;
+    textType:
+        | "body1"
+        | "body2"
+        | "h1"
+        | "h2"
+        | "h3"
+        | "h4"
+        | "h5"
+        | "h6"
+        | "subtitle1"
+        | "subtitle2";
+    style: ("italic" | "bold" | "strikethrough" | "underline")[];
     conditionalRender?: ParsedFunction;
 };
 
 export type RenderDividerItem = {
     supertype: "render";
     type: "divider";
-    child: AllRenderItems;
+    child?: AllRenderItems;
+    variant: "full" | "inset" | "middle";
     conditionalRender?: ParsedFunction;
 };
